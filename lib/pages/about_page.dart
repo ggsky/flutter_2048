5a848c850e4fbe732eaaf95c7ea1d143794fd112
liuyuqi-dellpc <liuyuqi.gov@msn.cn>
2023-04-30 02:46:10 +0800

    0

---
+++ lib/pages/about_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+/// Description: 关于界面
+/// Time       : 04/30/2023 Sunday
+/// Author     : liuyuqi.gov@msn.cn
+class AboutPage extends StatefulWidget {
+  AboutPage({Key key}) : super(key: key);
+
+  @override
+  _AboutPageState createState() => _AboutPageState();
+}
+
+class _AboutPageState extends State<AboutPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: Text("xx"),
+    );
+  }
+}
---
+++ lib/pages/guide_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+/// Description: 新手引导页
+/// Time       : 04/30/2023 Sunday
+/// Author     : liuyuqi.gov@msn.cn
+class GuidePage extends StatefulWidget {
+  GuidePage({Key key}) : super(key: key);
+
+  @override
+  _GuidePageState createState() => _GuidePageState();
+}
+
+class _GuidePageState extends State<GuidePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: child,
+    );
+  }
+}
